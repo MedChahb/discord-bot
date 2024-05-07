37711814c8095b767d006eaa12948f88d7bf5475
BOULAC NIZAR <nizar.boulac@etu.unistra.fr>
2024-05-07 10:40:09 +0200

    on nizar name ?

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace DiscordBot
     {
         static async Task Main(string[] args)
         {
+		//test if commit on nizar name
             Bot.Bot DiscordBot = new Bot.Bot(); // instance du Bot
             await Task.Delay(-1); // infinite delay -> bot stays connected
         }
